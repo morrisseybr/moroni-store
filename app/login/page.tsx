b73d1cfcec01f15e071a7bba21fdfec63912eb4f
Samuel Morrissey <samuel.morrissey@encontact.com.br>
2023-07-01 18:37:37 -0300

    Remoção do form de login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,9 @@
-import LoginForm from "./components/login-form";
-
 export default function LoginPage() {
   return (
     <main>
       <h1 className="">Moroni Store</h1>
       <span className="muted small">Área administrativa</span>
       <hr className="mt-3 mb-8" />
-      <LoginForm />
     </main>
   );
 }
