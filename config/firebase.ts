b357de433bd43e1beea955307b0a251d937304f6
Samuel Morrissey <samuel.morrissey@encontact.com.br>
2023-07-05 09:42:35 -0300

    Ajustes de configuração do firebase

--- config/firebase.ts
+++ config/firebase.ts
@@ -5,10 +5,24 @@ import {
   getApps,
 } from "firebase-admin/app";
 
-import serviceAccount from "./moroni-store-firebase-key.json";
+const FIREBASE_SERVICE_ACCOUNT = {
+  type: process.env.FIREBASE_SERVICE_ACCOUNT_TYPE,
+  project_id: process.env.FIREBASE_SERVICE_ACCOUNT_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_SERVICE_ACCOUNT_AUTH_URI,
+  token_uri: process.env.FIREBASE_SERVICE_ACCOUNT_TOKEN_URI,
+  auth_provider_x509_cert_url:
+    process.env.FIREBASE_SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url:
+    process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_SERVICE_ACCOUNT_UNIVERSE_DOMAIN,
+};
 
 if (getApps().length === 0) {
   initializeApp({
-    credential: cert(serviceAccount as ServiceAccount),
+    credential: cert(FIREBASE_SERVICE_ACCOUNT as ServiceAccount),
   });
 }
