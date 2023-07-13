f6a826712d049ba0c560b19d470beb35236dd38d
Samuel Morrissey <samuel.morrissey@encontact.com.br>
2023-07-12 21:10:04 -0300

    Prisma e NextAuth

--- config/nextauth.ts
+++ config/nextauth.ts
@@ -1,37 +1,63 @@
-import { NextAuthOptions } from "next-auth";
-import GoogleProvider, { GoogleProfile } from "next-auth/providers/google";
+import { NextAuthOptions, User } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import crypto from "crypto";
+
+const adminUsers = [
+  {
+    id: "1",
+    email: "samuel.morrissey@outlook.com",
+    passwordHash:
+      "481f6cc0511143ccdd7e2d1b1b94faf0a700a8b49cd13922a70b5ae28acaa8c5",
+  },
+  {
+    id: "2",
+    email: "email.do.simon@outlook.com",
+    passwordHash:
+      "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92",
+  },
+] as const;
 
 const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET as string,
   providers: [
-    GoogleProvider<GoogleProfile>({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      authorization: {
-        params: {
-          prompt: "consent",
-        },
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
       },
-      profile: (profile) => {
-        return {
-          id: profile.sub,
-          name: profile.name,
-          email: profile.email,
-          image: profile.picture,
-          allowed:
-            profile.email === "samuel.morrissey@gmail.com" ? true : false,
-        };
+      async authorize(credentials, req) {
+        const userExists = adminUsers.find((user) => {
+          user.email === credentials?.email;
+        });
+        const passwordMatches =
+          userExists?.passwordHash ===
+          crypto
+            .createHash("sha256")
+            .update(credentials?.password || "")
+            .digest("hex");
+
+        if (passwordMatches) {
+          const user: User = {
+            id: userExists.id,
+            email: userExists.email,
+            isAdmin: true,
+          };
+          return user;
+        } else {
+          return null;
+        }
       },
     }),
   ],
   callbacks: {
     jwt({ token, user }) {
-      if (user) token.allowed = user.allowed;
+      if (user) token.isAdmin = user.isAdmin;
       return token;
     },
     session({ session, token }) {
       if (session.user) {
-        session.user.allowed = token.allowed;
+        session.user.isAdmin = token.isAdmin || false;
       }
       return session;
     },
--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,15 +1,18 @@
-import NextAuth from "next-auth";
+import { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
-  /**
-   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
-   */
   interface Session {
     user: {
-      allowed: boolean;
+      isAdmin: boolean;
     } & DefaultSession["user"];
   }
   interface User {
-    allowed: boolean;
+    isAdmin: boolean;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    isAdmin?: boolean;
   }
 }
