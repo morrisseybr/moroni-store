aa86be863b9227a7c32f27270f3bc635612db039
Samuel Morrissey <samuel.morrissey@encontact.com.br>
2023-07-17 09:30:56 -0300

    Tentativa de resolução de uso de hook dentro de client component

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,14 +2,17 @@
 
 import { useToast } from "@/components/ui/use-toast";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function GlobalNotFound() {
   const router = useRouter();
   const { toast } = useToast();
-  toast({
-    title: "Page not found",
-    description: "The page you are looking for does not exist.",
-    variant: "destructive",
-  });
-  router.push("/");
+  useEffect(() => {
+    toast({
+      title: "Page not found",
+      description: "The page you are looking for does not exist.",
+      variant: "destructive",
+    });
+    router.push("/");
+  }, [router, toast]);
 }
