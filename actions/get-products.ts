28ba0dff7d1bc1662b33d0077d8170163777e6a2
Samuel Morrissey <samuel.morrissey@encontact.com.br>
2023-08-24 21:22:13 -0300

    Aumentando o pagesize

--- actions/get-products.ts
+++ actions/get-products.ts
@@ -5,5 +5,5 @@ export default async function getProducts(
   cursor: string | null
 ): Promise<Product[]> {
   const repo = new FirestoreProductRepository();
-  return await repo.list("name", 2, cursor ? new ProductId(cursor) : null);
+  return await repo.list("name", 10, cursor ? new ProductId(cursor) : null);
 }
